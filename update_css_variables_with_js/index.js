5c3ab9f03c1cb69f192fcc4140cf697c34617a28
agitserhat <76850140+agitserhat@users.noreply.github.com>
2021-01-03 19:26:55 +0300

    Add files via upload

---
+++ update_css_variables_with_js/index.js
@@ -0,0 +1,27 @@
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate() {
+  const suffix = this.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+
+
+//insert image
+
+window.addEventListener('load', function() {
+    document.querySelector('input[type="file"]').addEventListener('change', function() {
+        if (this.files && this.files[0]) {
+            var img = document.querySelector('img');  // $('img')[0]
+            img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+            img.onload = imageIsLoaded;
+        }
+    });
+  });
+  
+  function imageIsLoaded() { 
+   alert("Resim Seçildi!")
+  }
